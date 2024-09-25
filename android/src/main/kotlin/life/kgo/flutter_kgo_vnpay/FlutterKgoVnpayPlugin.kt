1734084ea3bc294ef9818943e3fad047f368c99c
vuanhvan <vuanhvan@hotmail.com>
2024-09-25 15:08:08 +0700

    Update FlutterKgoVnpayPlugin.kt

--- android/src/main/kotlin/life/kgo/flutter_kgo_vnpay/FlutterKgoVnpayPlugin.kt
+++ android/src/main/kotlin/life/kgo/flutter_kgo_vnpay/FlutterKgoVnpayPlugin.kt
@@ -24,6 +24,10 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
 import java.lang.ref.WeakReference
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.drawable.ColorDrawable;
+
 /** FlutterKgoVnpayPlugin */
 class FlutterKgoVnpayPlugin: FlutterPlugin, ActivityAware, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
